b4f68ae5b749c0b3fbaa2082a257a5fcdfb43367
Minami Nakagawa <marumegane1108@gmail.com>
2020-01-27 11:01:31 +0900

    first commit

---
+++ Assets/Scripts/hittest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hittest : MonoBehaviour
+{
+    public bool isHit;
+    public AudioClip sound1;
+    //AudioSource audioSource;
+
+    private void Start()
+    {
+        isHit = false;
+        //audioSource = GetComponent<AudioSource>();
+    }
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "testTag")
+        {
+            Destroy(other.gameObject);
+            //audioSource.PlayOneShot(sound1);
+            isHit = true;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/timerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timerController : MonoBehaviour
+{
+    public TextMesh textMesh;
+    float timer;
+    anmtest anm;
+    GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //textMesh = gameObject.AddComponent<TextMesh>();
+        textMesh.anchor = TextAnchor.LowerCenter;
+        textMesh.alignment = TextAlignment.Center;
+        door = GameObject.Find("Door");
+        anm = door.GetComponent<anmtest>();
+        textMesh.characterSize = 0.5f;
+        timer = 15.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(anm.anmbool == true)
+        {
+            textMesh.text = string.Format("条件をクリアしました",timer);
+            return;
+        }
+        if (timer < 0) timer = 0;
+        textMesh.text = string.Format("{0:000.0}", timer);
+        if (timer <= 0)
+        {
+            //Application.LoadLevel("FailureScene");
+        }
+    }
+}
